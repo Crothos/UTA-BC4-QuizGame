f50fd402774879d22b9ce1bbc59b1cfa606aba07
Crothos <s.g.totten@gmail.com>
2023-01-03 16:57:47 -0600

    Added README file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# UTA Bootcamp Challenge NUMBER
+# UTA Bootcamp Challenge NUMBER 4
 
-In this homework assignment from the University of Texas-Austin Bootcamp we were asked to ...
+In this homework assignment from the University of Texas-Austin Bootcamp we were asked to create a Quiz Game using elements of Javascript like timers, appends, document queries, and more.
+
+MIT License
---
+++ assets/pseudocode.js
@@ -0,0 +1,21 @@
+// User is present with 5 questions - array of objects
+    // Format for questions???
+        // Object that contains {question: string, Choices: array, Answer: string}
+        // Display question - loop
+        // append question
+        // append choices array
+        
+// user selects an answer (button/radio/checkbox)data-
+    // click event is on the parent container
+    // how to know which element was clicked(event.target)
+    // if answer is correct, display next question - access array of questions object
+    // if answer is incorrect, add 15 seconds to score and display next question
+
+    // When all questions are answered, display form to submit initials
+    // save form values score and initials to local storage
+
+// change to highscores HTML
+    // read values from localstorage
+    // append score values to page
+// window.location.href change to name of html for new page
+
