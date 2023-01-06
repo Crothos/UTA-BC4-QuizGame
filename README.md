4e9cb815e2e98ccadad5b940e6052ebb097ee3e9
Crothos <s.g.totten@gmail.com>
2023-01-06 16:17:49 -0600

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
-# UTA Bootcamp Challenge NUMBER 4
+# Quiz Game
 
-In this homework assignment from the University of Texas-Austin Bootcamp we were asked to create a Quiz Game using elements of Javascript like timers, appends, document queries, and more.
+## Description:
+This quiz game tests basic coding knowledge and gives you a score based on how quickly you finish the game. Wrong answers dock you 15 seconds.
+
+
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Mock-up](#mock-up)
+
+## Usage
+- Github Repo ()
+- Deployed URL ()
+
+## Credits
+- Starter Code/Repo by Josh Goeke (joshuagoeke).
+## License
 
 MIT License
+
+
+
+## Preview ##
+(./assets/images/CH4Prev.png)
--- assets/scorescript.js
+++ assets/scorescript.js
@@ -1,2 +1,23 @@
 var finalScore = localStorage.getItem("Score");
-console.log(finalScore);
+document.getElementById("score").innerHTML = "Your score is " + finalScore +"!";
+var initInput = document.querySelector("#initials");
+var submitButton = document.querySelector("#submit");
+var userInitSpan = document.querySelector("#user-initials");
+var userScoreSpan = document.querySelector("#user-score");
+
+renderScore();
+
+submitButton.addEventListener("click", function(event) {
+  event.preventDefault();
+
+  var init = document.querySelector("#initials").value;
+    localStorage.setItem("Initials", init);
+    renderScore();
+  }
+);
+
+function renderScore() {
+  var init = localStorage.getItem("Initials");
+  userInitSpan.textContent = init;
+  userScoreSpan.textContent = finalScore;
+}
