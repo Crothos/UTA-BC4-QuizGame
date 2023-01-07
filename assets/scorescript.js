a9ab4ff916d27985cbe579aa567801c4291efb5c
Crothos <s.g.totten@gmail.com>
2023-01-06 23:10:02 -0600

    Began adding high score div

--- assets/scorescript.js
+++ assets/scorescript.js
@@ -1,15 +1,14 @@
 var finalScore = localStorage.getItem("Score");
-document.getElementById("score").innerHTML = "Your score is " + finalScore +"!";
+document.getElementById("score").innerHTML = "Enter initials to see your score!";
 var initInput = document.querySelector("#initials");
 var submitButton = document.querySelector("#submit");
 var userInitSpan = document.querySelector("#user-initials");
 var userScoreSpan = document.querySelector("#user-score");
-
-renderScore();
+var highScores = document.querySelector("#highScores");
 
 submitButton.addEventListener("click", function(event) {
   event.preventDefault();
-
+  
   var init = document.querySelector("#initials").value;
     localStorage.setItem("Initials", init);
     renderScore();
@@ -20,4 +19,14 @@ function renderScore() {
   var init = localStorage.getItem("Initials");
   userInitSpan.textContent = init;
   userScoreSpan.textContent = finalScore;
+
+  var li = document.createElement("li");
+  li.textContent = init + " " + finalScore;
+  highScores.appendChild(li);
+  resetForm();
+}
+
+function resetForm() {
+  document.getElementById("initialForm").reset();
+
 }
